5865cd014bf105f996a11dabcea1162cabaeb272
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 18:08:57 +0200

    Update month.js
    rename identifieres

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,29 +1,16 @@
 // Get month number
 
-Months = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
-];
+Months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec',];
 
-Month = (s) => {
+const getMonthNum = (s) => {
   
-  l = Months.length;
+  const num_months = Months.length;
   
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < num_months; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   
   return -1;
 };
 
-module.exports = Month;
+module.exports = getMonthNum;
