4b66c3b77833a1602181b45bc826ca7f70ac2cc4
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 21:43:28 +0200

    Add const and space

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,15 +1,23 @@
 // Count types in an array
 
-types_ = function (s) {
-  types_ = {
+//Step 1
+//Add const and space
+
+'use strict';
+
+const types_ = (s) => {
+  
+  const types_ = {
     number: 0,
     string: 0,
     boolean: 0,
   };
+  
   for (i of s) {
     const t = typeof i;
     types_[t]++;
   }
+  
   s.push('string');
   return types_;
   s.length;
