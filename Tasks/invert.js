66943d92baf126addc39199b570f24258aa232b2
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 20:19:40 +0200

    Add space, replace forEach with for of, add empty arr

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -4,17 +4,21 @@
 //Delete unused and rename identifiers
 //Step 2
 //Add const 
+//Step 3 
+//Add empty arr
+//Replace forEach with for of
+//Add space
 
 'use strict';
 
 const invert = (arr) => {
- const rev_arr = Object.keys(arr, 4);
-  rev_arr.forEach((_, i) => {
-    rev_arr[i] = arr.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+ 
+ const rev_arr = []
+ 
+  for (const elm of inputArray) {
+    rev_arr.unshift(elm);
+  }
+ 
   return rev_arr;
 };
 
