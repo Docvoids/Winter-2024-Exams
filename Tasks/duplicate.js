1235cedced7cc3b1833dfd65e473f165c7c2ba56
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 19:59:44 +0200

    remove else

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -4,20 +4,20 @@
 //Rename
 //Step 2
 //Use const and space
+//Step 3
+//Remove else
 
 'use strict';
 
 const duplicate = (str, repeat_t) => {
   if (N <= 0) return [];
     
-  else {
     const text = [];
     for (let i = 0; i < repeat_t; i++) {
       text[i] = str;
     }
     
     return text;
-  }
 };
 
 module.exports = duplicate;
