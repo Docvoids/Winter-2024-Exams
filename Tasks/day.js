df093a7e2509b4d57169a6b7c98b94b12d2c1132
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 18:25:20 +0200

    add spaces

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,17 +4,21 @@
 //rename variables
 //Step 2
 //move let i in for
+//Step 3
+//add spaces
 
 'use strict'
 
 const Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (inpStr) => {
+  
   for (let i = 0; i < Days.length; i++) {
     if (inpStr.startsWith(Days[i].toLowerCase())) {
       return i + 1;
     }
   }
+  
   return -1;
 };
 
