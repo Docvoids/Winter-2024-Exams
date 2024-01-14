7a7b69077a6b0bb2f59dac814d1648c83a6712d7
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 20:37:38 +0200

    Rename func and variables

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,18 @@
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
-  key = '';
+//Step 1
+//Rename func and variables
+
+'use strict';
+
+let gen_pass = (alphabet, length) => {
+  const max_len = alphabet.length;
+  pass = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    Index = Math.floor(Math.random() * max_len);
+    pass = pass + alphabet[Index];
   }
-  return key;
+  return pass;
 };
 
 module.exports = GeneratePassword;
