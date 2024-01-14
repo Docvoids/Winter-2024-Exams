ea202f152c376796dd819e140686eca72919c7d8
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 20:45:16 +0200

    used const and add space

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,6 +1,11 @@
 // Split an array into two parts
 
-let split_Array = (index, array) => {
+//Step 1
+//used const and add space
+
+'use strict';
+
+const split_Array = (index, array) => {
   begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
