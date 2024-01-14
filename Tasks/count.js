349f4311683e8b3aa7e12e2e7a2acb38653068c2
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 21:18:27 +0200

    Use const and let

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,12 +2,15 @@
 
 //Step 1
 //Delete useless lines and add space
+//Step 2
+//Use const and let
 
 'use strict';
 
-count = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
+const count = (obj) => {
+  
+  let sum = 0;
+  const keys = Object.keys(obj);
   
   keys.forEach((key) => {
     const value = obj[key];
