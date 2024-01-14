cea0e2ba55a2558d1637ed14ac262dd0335f136d
Docvoids <143121183+Docvoids@users.noreply.github.com>
2024-01-14 18:20:47 +0200

    Update day.js
    rename variables

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,20 @@
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//Step 1
+//rename variables
 
-_parse_day_ = (s) => {
+'use strict'
+
+const Days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const parseDay = (inpStr) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < Days.length; i++) {
+    if (inpStr.startsWith(Days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
